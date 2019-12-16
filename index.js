6e33c329c5630f8bb497109e8aed1960c72f8c9d
Joseph Gilgen <gilgenlabs@gmail.com>
2019-12-16 11:34:06 -0700

    update: added graphql endpoint

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const { KhanOauth, KhanAPIWrapper } = require("./lib/api-base")
 const { KhanAPIWrapperV1Mixin } = require("./lib/api-v1")
 const { KhanAPIWrapperV2Mixin } = require("./lib/api-v2")
 const { KhanAPIWrapperInternalMixin } = require("./lib/api-internal")
+const { KhanAPIWrapperGraphQLMixin } = require("./lib/api-graphql")
 
 // combining all the methods across the different api versions.
-class _KhanAPIWrapper extends KhanAPIWrapperInternalMixin(
-  KhanAPIWrapperV2Mixin(KhanAPIWrapperV1Mixin(KhanAPIWrapper))
+class _KhanAPIWrapper extends KhanAPIWrapperGraphQLMixin(
+  KhanAPIWrapperInternalMixin(
+    KhanAPIWrapperV2Mixin(KhanAPIWrapperV1Mixin(KhanAPIWrapper))
+  )
 ) {}
 
 module.exports = { KhanOauth, KhanAPIWrapper: _KhanAPIWrapper }
